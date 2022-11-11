85cab3e848101673f790fba749304873b72d6c50
nikolai-chernolutskii <nikolai@site-now.online>
2022-11-11 15:28:14 +0200

    add: JS - reset value, add decimal

--- calculator/script.js
+++ calculator/script.js
@@ -8,6 +8,13 @@ function sendNumberValue(number) {
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
+function addDecimal() {
+    // If no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
+
 // Add event listeneres for numbers, operators, decimal btns
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0) /* this will be the case for our number btns because there is no class assigned to them */ {
@@ -15,7 +22,14 @@ inputBtns.forEach((inputBtn) => {
     } else if (inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => addDecimal());
     }
-
 });
+
+// Reset display
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+// Event listener for reset
+clearBtn.addEventListener('click', resetAll);
