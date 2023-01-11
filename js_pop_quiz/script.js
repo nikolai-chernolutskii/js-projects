a77ed7926a5714376b06c72d2e91fbf54b239b38
nikolai-chernolutskii <nikolai@site-now.online>
2023-01-11 15:02:30 +0200

    feat: create check f-n & disable/enable solution

--- js_pop_quiz/script.js
+++ js_pop_quiz/script.js
@@ -10,7 +10,7 @@ const explanation = document.getElementById('explanation');
 // Its value should be an object with a statement, true/false answer, and explanation
 const fact = {
     statement: '1 + 1 === 2',
-    answer: 'true',
+    answer: true,
     explanation: 'The plus operator performs the addition operation if applied to numbers'
 };
 
@@ -21,8 +21,13 @@ statement.textContent = fact.statement;
 // disable(button) should set the button element's attribute "disabled" to the value ""
 // enable(button) should remove the attribute "disabled" from the button element
 
+const disable = btn => btn.setAttribute("disabled", "");
+const enable = btn => btn.removeAttribute("disabled");
+
 // TODO 5: Declare an isCorrect function that compares a guess to the right answer
 // isCorrect(guess) should return true if the guess matches the fact's answer
+
+const isCorrect = guessString => guessString === fact.answer.toString();
 
 // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
 // TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
